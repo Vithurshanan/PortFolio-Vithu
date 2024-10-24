b1f1fecf5a632a83d3652768ec513f38d93967fd
Ananthakumar Vithurshanan <lithuvithu1109@gmail.com>
2024-10-24 17:50:52 +0530

    adding images

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -49,12 +49,12 @@ const page = () => {
             <div className="container mx-auto">
                 <div className="flex flex-col xl:flex-row gap-[30px]">
                     <div className="xl:w-[54%] order-2 xl:order-none">
-                        <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+                        <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl" >
                             <h3 className="text-4xl text-accent">Let's work together</h3>
                             <p className="text-white/60">
                                 Lorem ipsum dolor sit amet consectetur, adipisicing elit.  maxime cupiditate ducimus impedit odio molestias volup.
                             </p>
-                            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-6" >
                                 <Input type="firstName" placeholder="Firstname" />
                                 <Input type="lastname" placeholder="Lastname" />
                                 <Input type="email" placeholder="Email address" />
