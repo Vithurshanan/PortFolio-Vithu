26fca5e27efcfc4758e2ed9e147df6ba85d10fa2
Ananthakumar Vithurshanan <lithuvithu1109@gmail.com>
2024-12-17 10:03:01 +0530

    adjustment

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ import { FiDownload } from "react-icons/fi";
 export default function Home() {
   return (
     <section className="h-full">
-      <div className="container mx-auto h-full">
+      <div className="container mx-auto h-full ">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
           <div className="text-center xl:text-left order-2 xl:order-none">
@@ -17,7 +17,7 @@ export default function Home() {
             <h1 className="h1 mb-6">
               Hello, I'm <br /> <span className="text-accent">Vithurshanan</span>
             </h1>
-            <p className="max-w-[500px] mb-9 text-white/80">
+            <p className="max-w-[400px] mb-9 text-white/80">
               I specialize in creating seamless, cutting-edge digital experiences,
               leveraging my expertise in a wide range of programming languages and advanced technologies.
             </p>
@@ -37,10 +37,10 @@ export default function Home() {
           <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
-
         </div>
+        <Stats />
       </div>
-      <Stats />
+     
     </section>
   );
 }
